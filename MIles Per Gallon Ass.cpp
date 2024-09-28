9c498829993a29f6e9c58312c6e7a4a8fa4b6ad4
WtRhoads <wtrhoads@icloud.com>
2024-09-28 16:05:45 -0400

    Add files via upload

---
+++ MIles Per Gallon Ass.cpp
@@ -0,0 +1,34 @@
+// MIles Per Gallon Ass.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ //show miles pergallon ration: miles per gallon = miles driven/gallons of gas used
+	//"Miles Per Gallon"=mpg
+	//"Miles driven"=md
+	//"Gallons of gas used"=gog
+	float mpg;
+	float md;
+	float gog;
+		md = 375;
+		gog = 15;
+		mpg = md / gog;
+	//Display mpg = md/gog
+		cout << "When driven " << md << " miles, " << " the vehicle will use " << gog << " gallons of gas.\n" << "Therefore the vehicle will get " << mpg << " miles per gallon.";
+
+		
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
